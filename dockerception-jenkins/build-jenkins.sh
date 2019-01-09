a0630c220a4aa392ff200ad1898fe02070e7bb4b
david.williams <xpositivityx@gmail.com>
2019-01-09 14:55:11 -0800

    adding creation of jenkins home to build script. mkaing jenkins tty to run in background

--- dockerception-jenkins/build-jenkins.sh
+++ dockerception-jenkins/build-jenkins.sh
@@ -4,6 +4,8 @@
 sudo useradd jenkins
 sudo usermod -aG docker jenkins
 
+mkdir /var/jenkins_home
+chmod -R 777 /var/jenkins_home
 
 docker build \
     -t jenkins:local \
--- dockerception-jenkins/run-jenkins.sh
+++ dockerception-jenkins/run-jenkins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker run \
-    -d --rm \
+    -d -ti --rm \
+    --name jenkins \
     -p 8080:8080 -p 50000:50000 \
     -v /var/jenkins_home:/var/jenkins_home \
     -v /var/run/docker.sock:/var/run/docker.sock \
